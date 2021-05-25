96c779ae049013faaae761668cf5a63258d117fc
moayada <moayada@kth.se>
2021-05-26 00:31:05 +0200

    Added the service layer

--- src/main/java/ddns/alkhatib/passwordGeneratorAPI/API/API.java
+++ src/main/java/ddns/alkhatib/passwordGeneratorAPI/API/API.java
@@ -1,15 +1,20 @@
 package ddns.alkhatib.passwordGeneratorAPI.API;
-
+import ddns.alkhatib.passwordGeneratorAPI.service.PasswordGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "password-gen")
 public class API {
 
+    @Autowired
+    private PasswordGenerator pg;
+
+
     @GetMapping
-    public String getPass(){
-        return "password";
+    @RequestMapping(path = "password-gen")
+    public String getPassword(){
+        return pg.getPass();
     }
 }
---
+++ src/main/java/ddns/alkhatib/passwordGeneratorAPI/service/PasswordGenerator.java
@@ -0,0 +1,11 @@
+package ddns.alkhatib.passwordGeneratorAPI.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordGenerator {
+
+    public String getPass(){
+        return "Hello world";
+    }
+}
