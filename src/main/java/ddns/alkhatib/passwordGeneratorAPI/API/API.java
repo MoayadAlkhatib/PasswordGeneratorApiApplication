07c6cac85af43707ae0f3b2bac8871670272b9cf
moayada <moayada@kth.se>
2021-05-26 02:36:27 +0200

    Added the getPass with parameter function

--- src/main/java/ddns/alkhatib/passwordGeneratorAPI/API/API.java
+++ src/main/java/ddns/alkhatib/passwordGeneratorAPI/API/API.java
@@ -2,6 +2,7 @@ package ddns.alkhatib.passwordGeneratorAPI.API;
 import ddns.alkhatib.passwordGeneratorAPI.service.PasswordGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class API {
     @RequestMapping(path = "password-gen")
     public String getPassword(){
             return pg.getPass();
+    }
+
+    @GetMapping("password-gen/{id}")
+    public String getPassword(@PathVariable int id){
+        return pg.getPass(id);
     }
 }
